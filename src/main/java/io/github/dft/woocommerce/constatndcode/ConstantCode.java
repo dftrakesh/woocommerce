58ff74918d1f4b7023454c11027f23cf9f12fc70
dhruv.modi <dhruv.modi@dakshyafreondtech.com>
2023-03-02 11:07:40 +0530

    Created New WooCommerce project for SDK methods and created POJO's for webhook,update inventory and authentication. Created one file of constantcode and class for deserialization.

--- src/main/java/io/github/dft/woocommerce/constatndcode/ConstantCode.java
+++ src/main/java/io/github/dft/woocommerce/constatndcode/ConstantCode.java
@@ -1,10 +1,6 @@
 package io.github.dft.woocommerce.constatndcode;
 
 public interface ConstantCode {
-    String GET_METHOD = "GET";
-    String POST_METHOD = "POST";
-    String PUT_METHOD = "PUT";
-    String DELETE_METHOD = "DELETE";
     String API_BASE_END_POINT = "https://www.woora.a2hosted.com/wp-json/wc/v3";
     String ORDER_ENDPOINT = "/orders";
     String REFUND_ENDPOINT = "/refunds";
@@ -24,8 +20,6 @@ public interface ConstantCode {
     String CONTENT_TYPE_VALUE = "application/json";
     String CONSUMER_KEY = "ck_b8db0d1eb55cac61f8edc04cf62aaa8656888d9b";
     String CONSUMER_SECRET = "cs_f5efc84e1d30d48ea94fd6b452dae4a53dcdfb82";
-
-
     int MAX_ATTEMPTS = 50;
     int TIME_OUT_DURATION = 3000;
     int TOO_MANY_REQUEST_EXCEPTION_CODE = 429;
