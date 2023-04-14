da026d195d6a206de0f8d623ab9cbd746b1eb2a7
Hinali.Sharma <hinali.sharma@dakshyafreondtech.com>
2023-04-14 12:47:15 +0530

    RAG-192 Changes in Product.java, WooCommerceReports.java, AccessCredential.java Files. Remove HttpConstants.java. Change version in pom.xml.

--- src/main/java/io/github/dft/woocommerce/model/authenticationapi/AccessCredential.java
+++ src/main/java/io/github/dft/woocommerce/model/authenticationapi/AccessCredential.java
@@ -1,5 +1,7 @@
 package io.github.dft.woocommerce.model.authenticationapi;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AccessCredential {
     private String storeDomain;
     private String keyId;
