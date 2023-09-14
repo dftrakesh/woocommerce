1e86794371445ba7145f336625a935aa33bb4a57
dishant.loriya <dishant.loriya@dakshyafreondtech.com>
2023-09-14 17:59:31 +0530

    GODAM-28: add shipment tracking api

--- src/main/java/io/github/dft/woocommerce/model/shipmenttrackingapi/Tracking.java
+++ src/main/java/io/github/dft/woocommerce/model/shipmenttrackingapi/Tracking.java
@@ -1,12 +1,15 @@
 package io.github.dft.woocommerce.model.shipmenttrackingapi;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Tracking {
+    private String trackingLink;
     private String trackingNumber;
     private String trackingProvider;
 }
