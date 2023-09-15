1fd23ee0779440f13f09d504755ede0efef53b33
dishant.loriya <dishant.loriya@dakshyafreondtech.com>
2023-09-15 10:51:19 +0530

    GODAM-28: add shipment tracking api

--- src/main/java/io/github/dft/woocommerce/WooCommerceShipmentTracking.java
+++ src/main/java/io/github/dft/woocommerce/WooCommerceShipmentTracking.java
@@ -3,6 +3,7 @@ package io.github.dft.woocommerce;
 import io.github.dft.woocommerce.model.authenticationapi.AccessCredential;
 import io.github.dft.woocommerce.model.shipmenttrackingapi.Tracking;
 import io.github.dft.woocommerce.model.shipmenttrackingapi.TrackingResponse;
+import io.github.dft.woocommerce.model.shipmenttrackingapi.TrackingResponseList;
 import lombok.SneakyThrows;
 import java.net.URI;
 import java.net.http.HttpRequest;
@@ -13,6 +14,14 @@ public class WooCommerceShipmentTracking extends WooCommerceSdk {
 
     public WooCommerceShipmentTracking(AccessCredential accessCredential) {
         super(accessCredential);
+    }
+
+    @SneakyThrows
+    public TrackingResponseList getShipmentTracking(String orderId) {
+        URI uri = shipmentTrackingBaseUrl(SHIPMENT_TRACKING_ENDPOINT.replace("orderId", orderId));
+        HttpRequest request = get(uri);
+
+        return getRequestWrapped(request, TrackingResponseList.class);
     }
 
     @SneakyThrows
---
+++ src/main/java/io/github/dft/woocommerce/model/shipmenttrackingapi/TrackingResponseList.java
@@ -0,0 +1,8 @@
+package io.github.dft.woocommerce.model.shipmenttrackingapi;
+
+import lombok.Data;
+import java.util.ArrayList;
+
+@Data
+public class TrackingResponseList extends ArrayList<TrackingResponse> {
+}
