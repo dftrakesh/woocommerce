2dfef115e7adbf0207181453d7071ec3e987ce10
dishant.loriya <dishant.loriya@dakshyafreondtech.com>
2023-09-22 19:08:31 +0530

    GODAM-27: update createShipmentTracking for custom tracking provider

--- src/main/java/io/github/dft/woocommerce/constants/ConstantCodes.java
+++ src/main/java/io/github/dft/woocommerce/constants/ConstantCodes.java
@@ -7,6 +7,7 @@ public interface ConstantCodes {
     Map<String, String> TRACKING_PROVIDER_LINK_MAP = new HashMap<>(){
         {
             put("EKART", "https://ekartlogistics.com/shipmenttrack/{trackingNumber}");
+            put("SHADOWFAX", "https://tracker.shadowfax.in/");
         }
     };
 }
